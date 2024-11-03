23fb86450acb27a428ae5fae77646ebec8a99951
Beby Nostalgia Lahagu <bebynlahagu20@gmail.com>
2024-11-03 19:28:26 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ let menuBar = document.querySelector('#menuBtn');
 menuBar.onclick = () => {
     navBar.classList.toggle('active');
 }
+
+/*scroll dan navbar sticky */
+window.onscroll = () => {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.screenY > 100);
+}
