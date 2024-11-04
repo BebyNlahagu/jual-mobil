2a6be199eec9f8e99f6f09de30be11ecdaef983b
Beby Nostalgia Lahagu <bebynlahagu20@gmail.com>
2024-11-04 16:22:58 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,22 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.screenY > 100);
 }
+
+/*swipper*/
+var swiper = new Swiper(".myHome", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
